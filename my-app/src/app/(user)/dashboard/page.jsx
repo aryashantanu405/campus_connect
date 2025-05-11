c9db1607099ed34e9197b9bb59d46f83fca12631
aryashantanu405 <aryas.ug23.ec@nitp.ac.in>
2025-05-11 23:36:06 +0530

    backend structure

--- my-app/src/app/(user)/dashboard/page.jsx
+++ my-app/src/app/(user)/dashboard/page.jsx
@@ -1,8 +1,9 @@
 'use client'
-
+import UserFlexCard from "@/components/ui/usercard"
 const page = () => {
   return (
     <div className="text-xl text-white bg-black font-extrabold text-center">
+      <UserFlexCard/>
      {/* lost or found something...so you can click button to go to the lost-found page and post there */}
 
      {/* go to community for the lastest updates */}
