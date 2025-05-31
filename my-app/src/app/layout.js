9738929dca0055a2f5b6e8769e50fdf95ae593d0
aryashantanu405 <aryas.ug23.ec@nitp.ac.in>
2025-05-31 21:38:41 +0530

    added analytics tracking

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,6 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
-
+import {Analytics} from '@vercel/analytics/next';
 export default function RootLayout({ children }) {
   const pkey= process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
   return (
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
       <body
       >
         {children}
+        <Analytics />
       </body>
     </html>
     </ClerkProvider>
